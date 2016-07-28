88e6fa7afefde954b46d87005bd210f5f755cf2e
Katie Dektar <katydek@google.com>
2016-07-28 10:43:09 -0700

    Fix permissions loop from support lib upgrade.
    BUG=30267890,29074530
    
    Change-Id: I9ec569243ace7acab6544cde4914496616761ba0
    (cherry picked from commit 03503dde5e81a8e9d59c0f1a4f52573c0a7b1a9b)

--- OpenScienceJournal/whistlepunk_library/src/main/java/com/google/android/apps/forscience/whistlepunk/SensorCardPresenter.java
+++ OpenScienceJournal/whistlepunk_library/src/main/java/com/google/android/apps/forscience/whistlepunk/SensorCardPresenter.java
@@ -132,6 +132,7 @@ public class SensorCardPresenter {
     private String mTabSelectedFormat;
     private final Fragment mParentFragment;
     private PopupMenu mPopupMenu;
+    private boolean mAllowRetry = true;
 
     private OptionsListener mCommitListener = new OptionsListener() {
         @Override
@@ -263,7 +264,7 @@ public class SensorCardPresenter {
         mCardViewHolder.statusRetryButton.setVisibility(View.GONE);
         if (mHasError) {
             // An error
-            if (mRetryClickListener != null) {
+            if (mRetryClickListener != null && mAllowRetry) {
                 mCardViewHolder.statusRetryButton.setVisibility(View.VISIBLE);
                 mCardViewHolder.statusRetryButton.setOnClickListener(mRetryClickListener);
             }
@@ -746,7 +747,7 @@ public class SensorCardPresenter {
     // Selects the new sensor if it is different from the old sensor or if no sensor is currently
     // selected.
     private void trySelectingNewSensor(String newSensorId, String oldSensorId) {
-        if (mCurrentSource == null || !TextUtils.equals(newSensorId, oldSensorId)) {
+        if ((mCurrentSource == null && !mHasError) || !TextUtils.equals(newSensorId, oldSensorId)) {
             mOnSensorClickListener.onSensorClicked(newSensorId);
         }
     }
@@ -1197,7 +1198,8 @@ public class SensorCardPresenter {
                 });
     }
 
-    void setConnectingUI(String sensorId, boolean hasError, Context context) {
+    void setConnectingUI(String sensorId, boolean hasError, Context context, boolean allowRetry) {
+        mAllowRetry = allowRetry;
         SensorAppearance appearance = mAppearanceProvider.getAppearance(sensorId);
         setUiForConnectingNewSensor(sensorId,
                 appearance.getSensorDisplayName(context), appearance.getUnits(context), hasError);
