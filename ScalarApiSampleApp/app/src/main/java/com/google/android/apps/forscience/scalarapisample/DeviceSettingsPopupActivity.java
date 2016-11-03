701cc751e22de0600c273cc470ad7f1d64b25509
David Saff <david@saff.net>
2016-11-03 10:38:31 -0400

    Rename samplegyroprovider to scalarapisample
    BUG=32575766
    
    Change-Id: I3b667818ee7393b0653a8e76bae52d720473dc6e

--- ScalarApiSampleApp/app/src/main/java/com/google/android/apps/forscience/scalarapisample/DeviceSettingsPopupActivity.java
+++ ScalarApiSampleApp/app/src/main/java/com/google/android/apps/forscience/scalarapisample/DeviceSettingsPopupActivity.java
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package com.google.android.apps.forscience.samplegyroprovider;
+package com.google.android.apps.forscience.scalarapisample;
 
 import android.app.Activity;
 import android.app.PendingIntent;
