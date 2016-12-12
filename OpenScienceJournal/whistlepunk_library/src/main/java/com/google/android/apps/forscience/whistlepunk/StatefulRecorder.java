4a421a75dbbe2f5abe4ad04a7ece87e5e19011ed
David Saff <david@saff.net>
2016-12-12 16:00:31 -0500

    Allow retry to re-start a sensor even when it's recording.
    [Note: Only change in StatefulRecorder during the move is in method startObserving]
    
    BUG=33214956
    
    Change-Id: Id3363286e6f3d3c37f391c6545db2602ca63d6c1

---
+++ OpenScienceJournal/whistlepunk_library/src/main/java/com/google/android/apps/forscience/whistlepunk/StatefulRecorder.java
@@ -0,0 +1,123 @@
+/*
+ *  Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.google.android.apps.forscience.whistlepunk;
+
+import android.support.annotation.VisibleForTesting;
+
+import com.google.android.apps.forscience.javalib.Delay;
+import com.google.android.apps.forscience.javalib.MaybeConsumer;
+import com.google.android.apps.forscience.javalib.Scheduler;
+import com.google.android.apps.forscience.javalib.Success;
+import com.google.android.apps.forscience.whistlepunk.sensorapi.ReadableSensorOptions;
+import com.google.android.apps.forscience.whistlepunk.sensorapi.SensorRecorder;
+
+@VisibleForTesting
+public class StatefulRecorder {
+    private boolean mObserving = false;
+    private boolean mRecording = false;
+    private final SensorRecorder mRecorder;
+    private final Scheduler mScheduler;
+    private Delay mStopDelay;
+    private boolean mForceHasDataForTesting = false;
+    private Runnable mStopRunnable;
+
+    StatefulRecorder(SensorRecorder recorder, Scheduler scheduler, Delay stopDelay) {
+        mRecorder = recorder;
+        mScheduler = scheduler;
+        mStopDelay = stopDelay;
+    }
+
+    public void startObserving() {
+        cancelCurrentStopRunnable();
+        if (!mObserving) {
+            mRecorder.startObserving();
+        }
+        mObserving = true;
+    }
+
+    private void cancelCurrentStopRunnable() {
+        if (mStopRunnable != null) {
+            mScheduler.unschedule(mStopRunnable);
+            mStopRunnable = null;
+        }
+    }
+
+    public void stopObserving() {
+        if (mStopRunnable != null) {
+            // Already stopping.
+            return;
+        }
+        Runnable stopRunnable = new Runnable() {
+            @Override
+            public void run() {
+                mObserving = false;
+                maybeStopObserving();
+            }
+        };
+        if (mScheduler != null && !mStopDelay.isZero()) {
+            mStopRunnable = stopRunnable;
+            mScheduler.schedule(mStopDelay, mStopRunnable);
+        } else {
+            stopRunnable.run();
+        }
+    }
+
+    /**
+     * @param runId id for the run that is starting, to allow out-of-band data saving
+     */
+    public void startRecording(String runId) {
+        mRecorder.startRecording(runId);
+        mRecording = true;
+    }
+
+
+    void stopRecording(MaybeConsumer<Success> onSuccess) {
+        mRecorder.stopRecording(onSuccess);
+        mRecording = false;
+    }
+
+    // The spec for SensorRecorder says that once you stopObserving, the recorder should
+    // shut down.  Unless we decide to change that (and retrofit all current sensors to that
+    // new design), we should only _actually_ call stopObserving once we're no longer
+    // observing OR recording.
+    private void maybeStopObserving() {
+        if (!isStillRunning()) {
+            mRecorder.stopObserving();
+        }
+    }
+
+    public boolean isStillRunning() {
+        return mObserving || mRecording;
+    }
+
+    public void applyOptions(ReadableSensorOptions settings) {
+        mRecorder.applyOptions(settings);
+    }
+
+    public boolean hasRecordedData() {
+        return mRecorder.hasRecordedData() || mForceHasDataForTesting;
+    }
+
+    // TODO: Find a better way to fake force add data than this method.
+    @VisibleForTesting
+    void forceHasDataForTesting(boolean hasDataForTesting) {
+        mForceHasDataForTesting = hasDataForTesting;
+    }
+
+    public boolean isRecording() {
+        return mRecording;
+    }
+}
